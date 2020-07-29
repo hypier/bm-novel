03f387669baa44cbd0cd31e2f1686292f2bd8a53
heyong <hypier@qq.com>
2020-07-29 10:24:37 +0800

    在没有加recover的影响下，去掉panic

--- internal/infrastructure/postgres/db.go
+++ internal/infrastructure/postgres/db.go
@@ -14,7 +14,13 @@ var DefaultDB *sqlx.DB
 
 func init() {
 	config.LoadConfig()
-	DefaultDB, _ = connectMysql()
+	db, err := connectMysql()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	DefaultDB = db
 }
 
 func connectMysql() (*sqlx.DB, error) {
@@ -23,14 +29,14 @@ func connectMysql() (*sqlx.DB, error) {
 
 	db, err := sqlx.Open("postgres", dsn)
 	if err != nil {
-		panic(errors.WithMessage(err, "failed to connect to user db"))
+		return nil, errors.WithMessage(err, "failed to connect to user db")
 	}
 
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(3)
 	err = db.Ping()
 	if err != nil {
-		panic(errors.WithMessage(err, "failed to connect to user db"))
+		return nil, errors.WithMessage(err, "failed to connect to user db")
 	}
 	return db, nil
 }
