6a4206f24cc3ded2781affca5cfa2a386a51ffeb
heyong <hypier@qq.com>
2020-08-04 10:08:19 +0800

    api db

--- docs/schema.sql
+++ docs/schema.sql
@@ -32,114 +32,116 @@ create table if not exists public.permission
 alter table public.permission owner to postgres;
 
 
-create table if not exists novel
+create table if not exists public.novel
 (
-	novel_id uuid not null
-		constraint novel_pkey
-			primary key,
-	assigned_chapters integer,
-	chapters_count integer,
-	chief_editor_id varchar(255),
-	create_at timestamp,
-	novel_title varchar(255),
-	responsible_editor_id varchar(255),
-	update_at timestamp,
-	words_count integer
+    novel_id uuid not null
+        constraint novel_pkey
+            primary key,
+    assigned_chapters integer,
+    chapters_count integer,
+    chief_editor_id varchar(255),
+    create_at timestamp,
+    novel_title varchar(255)
+        constraint uk_novel_novel_title
+            unique,
+    responsible_editor_id varchar(255),
+    update_at timestamp,
+    words_count integer
 );
 
-alter table novel owner to postgres;
+alter table public.novel owner to postgres;
 
-create table if not exists chapter
+create table if not exists public.chapter
 (
-	chapter_id uuid not null
-		constraint chapter_pkey
-			primary key,
-	chapter_no integer,
-	chapter_title varchar(255),
-	create_at timestamp,
-	flag integer,
-	out_source_id uuid,
-	update_at timestamp,
-	words_count integer,
-	novel_novel_id uuid
-		constraint fkqpv7518o0l6ccy6d761r1mhqm
-			references novel
+    chapter_id uuid not null
+        constraint chapter_pkey
+            primary key,
+    chapter_no integer,
+    chapter_title varchar(255),
+    create_at timestamp,
+    flag integer,
+    out_source_id uuid,
+    update_at timestamp,
+    words_count integer,
+    novel_id uuid
+        constraint fk_chapter_novel_id
+            references public.novel
 );
 
-alter table chapter owner to postgres;
+alter table public.chapter owner to postgres;
 
-create table if not exists episode
+create table if not exists public.episode
 (
-	episode_id uuid not null
-		constraint episode_pkey
-			primary key,
-	create_at timestamp,
-	episode_name varchar(255),
-	episode_no integer,
-	status integer,
-	update_at timestamp,
-	words_count integer,
-	novel_novel_id uuid
-		constraint fkp3ftsa7ujvlin86bo0g0sas72
-			references novel
+    episode_id uuid not null
+        constraint episode_pkey
+            primary key,
+    create_at timestamp,
+    episode_name varchar(255),
+    episode_no integer,
+    status integer,
+    update_at timestamp,
+    words_count integer,
+    novel_id uuid
+        constraint fk_episode_novel_id
+            references public.novel
 );
 
-alter table episode owner to postgres;
+alter table public.episode owner to postgres;
 
-create table if not exists novel_role
+create table if not exists public.novel_role
 (
-	role_id uuid not null
-		constraint novel_role_pkey
-			primary key,
-	age varchar(255),
-	characters varchar(255),
-	create_at timestamp,
-	gender varchar(255),
-	role_class varchar(255),
-	role_name varchar(255),
-	update_at timestamp
+    role_id uuid not null
+        constraint novel_role_pkey
+            primary key,
+    age varchar(255),
+    characters varchar(255),
+    create_at timestamp,
+    gender varchar(255),
+    role_class varchar(255),
+    role_name varchar(255),
+    update_at timestamp
 );
 
-alter table novel_role owner to postgres;
+alter table public.novel_role owner to postgres;
 
-create table if not exists novel_setting
+create table if not exists public.novel_setting
 (
-	setting_id uuid not null
-		constraint novel_setting_pkey
-			primary key,
-	chapter_has_prefix boolean,
-	chapter_no_format integer,
-	chapter_prefix varchar(255),
-	episode_words_max integer,
-	episode_words_min integer,
-	novel_novel_id uuid
-		constraint fkje8lobfhjwsia1o624d0whn5h
-			references novel
+    setting_id uuid not null
+        constraint novel_setting_pkey
+            primary key,
+    chapter_has_prefix boolean,
+    chapter_no_format integer,
+    chapter_prefix varchar(255),
+    episode_words_max integer,
+    episode_words_min integer,
+    novel_id uuid
+        constraint fk_novel_setting_novel_id
+            references public.novel
 );
 
-alter table novel_setting owner to postgres;
+alter table public.novel_setting owner to postgres;
 
-create table if not exists paragraph
+create table if not exists public.paragraph
 (
-	paragraph_id uuid not null
-		constraint paragraph_pkey
-			primary key,
-	content varchar(5000),
-	create_at timestamp,
-	next integer,
-	prev integer,
-	update_at timestamp,
-	word_count integer,
-	chapter_chapter_id uuid
-		constraint fknxb6bf0oifdq62wo2yswd2wkp
-			references chapter,
-	episode_episode_id uuid
-		constraint fkj527tq0er85i0h3k072rn1nsk
-			references episode,
-	novel_role_role_id uuid
-		constraint fkcyfx068pvr5fkfdr51bs5lw90
-			references novel_role
+    paragraph_id uuid not null
+        constraint paragraph_pkey
+            primary key,
+    content varchar(5000),
+    create_at timestamp,
+    next integer,
+    prev integer,
+    update_at timestamp,
+    word_count integer,
+    chapter_id uuid
+        constraint fk_paragraph_chapter_id
+            references public.chapter,
+    episode_id uuid
+        constraint fk_paragraph_episode_id
+            references public.episode,
+    role_id uuid
+        constraint fk_paragraph_role_id
+            references public.novel_role
 );
 
-alter table paragraph owner to postgres;
+alter table public.paragraph owner to postgres;
 
---
+++ internal/controller/novel/novel_controller.go
@@ -0,0 +1,24 @@
+package novel
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetNovels(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	params := struct {
+		// 角色名
+		RoleCode []string `json:"role_code"`
+		// 姓名
+		RealName string `json:"real_name"`
+		// 当前页码
+		PageIndex int `json:"page_index"`
+		// 每页数量
+		PageSize int `json:"page_size"`
+	}{}
+
+	fmt.Println(params)
+
+}
